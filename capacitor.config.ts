bd3e0341908ad9bebdebea105104715d62ea8006
alvl1011 <alvl1011@h-ka.de>
2022-06-06 20:16:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dicenet-prototype',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
